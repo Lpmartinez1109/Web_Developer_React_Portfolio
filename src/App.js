0ed283f6c74423d9f161fe63efab63ac7134d07d
LPMartinez <lpmartinez1109@gmail.com>
2020-09-14 10:50:06 -0500

    edits made to App.js file, Components added to link

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <h1>Hello World!</h1>
-  );
+import React, { Component } from 'react';
+import Header from './components/header/header';
+import About from './components/about/about';
+import Resume from './components/resume/resume';
+import Portfolio from './components/portfolio/portfolio';
+import Contact from './components/contact/contact';
+import Footer from './components/footer/footer';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <About />
+        <Resume />
+        <Portfolio />
+        <Contact />
+        <Footer />
+      </div>
+    );
+  }
 }
-
 export default App;
