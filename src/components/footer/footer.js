6dc11af40a95037fc4dadb5063c46ce58314bc1a
LPMartinez <lpmartinez1109@gmail.com>
2020-09-15 10:12:30 -0500

    Footer component edited for repo

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -18,14 +18,13 @@ export default class Footer extends Component {
               <li><a href="#"><i className="fa fa-skype" /></a></li>
             </ul>
             <ul className="copyright">
-              <li>© Copyright 2014 CeeVee</li>
+              <li>© Copyright 2020</li>
               <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>   
             </ul>
           </div>
           <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open" /></a></div>
         </div>
       </footer> {/* Footer End*/}
-    );
             </React.Fragment>
         )
     }
