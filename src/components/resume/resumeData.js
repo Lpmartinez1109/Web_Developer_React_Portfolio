9aabcfbaedcf9469bf98a4a822ada4d463c292f1
LPMartinez <lpmartinez1109@gmail.com>
2020-09-16 08:45:52 -0500

    resumeData js in works. Filling all components of the resume in progress

--- src/components/resume/resumeData.js
+++ src/components/resume/resumeData.js
@@ -1,6 +1,20 @@
 let resumeData = {
-    "imagebaseurl" : "http://localhost:3000/",
+    "imagebaseurl" : "https://enigmatic-bayou-00328.herokuapp.com/",
     "name": "Luis Martinez",
-    "role" : "Web Developer"
+    "role" : "Web Developer",
+    "linkedinID" : "https://www.linkedin.com/in/luis-martinezjr/",
+    "aboutMe" : "I help clients create and develop the websites and apps they need in their day-to-day lives. After graduating with a bachelor’s degree in Journalism from Eastern Illinois University and obtaining certification in Full Stack Web Development from Northwestern University-Chicago Campus, I worked on and created multiple different projects utilizing a number of different coding languages including HTML, CSS, Javascript, React.js, and MongoDB/Mongoose.",
+    "socialLinks" : [
+        {
+            "name" : "linkedin",
+             "url" : "https://www.linkedin.com/in/luis-martinezjr/",
+             "className" : "fa fa-linkedin"
+        },
+        {
+            "name" : "github",
+            "url" : "https://github.com/Lpmartinez1109",
+            "className" : "fa fa-github"
+        }
+    ]
 }
 export default resumeData;
