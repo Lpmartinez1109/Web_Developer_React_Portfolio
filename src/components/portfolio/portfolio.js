84cb58e638f473c5e5afbcaf92e1a46b6b9424ab
LPMartinez <lpmartinez1109@gmail.com>
2020-09-21 08:32:44 -0500

    Conditional added to component in order to add projects information into portfolio component

--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -1,6 +1,25 @@
 import React, {Component} from 'react';
 export default class Portfolio extends Component{
     render(){
+        if(this.props.data){
+          var works = this.props.data.works.map(function(works){
+            var worksImages = 'images/portfolio'+works.image;
+            return <div key={works.title} className="columns portfolio-item">
+              <div className="item-wrap">
+                <a href={works.url} title={works.title}>
+                  <img alt={works.title} src={worksImages} />
+                  <div className="overlay">
+                    <div className="portfolio-item-meta">
+                      <h5>{works.title}</h5>
+                      <p>{works.category}</p>
+                    </div>
+                  </div>
+                  <div className="link-icon"><i className="fa fa-link"></i></div>
+                </a>
+              </div>
+            </div>
+          })
+        }
         return(
             <React.Fragment>
                 <section id="portfolio">
