a10150bb510c2e27e5f5142e8f343c160d82b1d5
LPMartinez <lpmartinez1109@gmail.com>
2020-10-05 14:19:30 -0500

    ResumeData file edit for future use. Should allow for easier editing in future

--- src/components/resume/resumeData.js
+++ src/components/resume/resumeData.js
@@ -3,7 +3,7 @@ let resumeData = {
     "name": "Luis Martinez",
     "role" : "Web Developer",
     "linkedinID" : "https://www.linkedin.com/in/luis-martinezjr/",
-    "aboutMe" : "I help clients create and develop the websites and apps they need in their day-to-day lives. After graduating with a bachelor’s degree in Journalism from Eastern Illinois University and obtaining certification in Full Stack Web Development from Northwestern University-Chicago Campus, I worked on and created multiple different projects utilizing a number of different coding languages including HTML, CSS, Javascript, React.js, and MongoDB/Mongoose.",
+    "roleDescription" : "I'm a Chicago based web developer working hard to create awesome and effective visual identities for companies of all sizes around the globe. Let's start scrolling and learn more about me.",
     "socialLinks" : [
         {
             "name" : "linkedin",
@@ -14,6 +14,124 @@ let resumeData = {
             "name" : "github",
             "url" : "https://github.com/Lpmartinez1109",
             "className" : "fa fa-github"
+        }
+    ],
+    "aboutme": "I help clients create and develop the websites and apps they need in their day-to-day lives. After graduating with a bachelor’s degree in Journalism from Eastern Illinois University and obtaining certification in Full Stack Web Development from Northwestern University-Chicago Campus, I worked on and created multiple different projects utilizing a number of different coding languages including HTML, CSS, Javascript, React.js, and MongoDB/Mongoose. My technical skills combined with my experience in collaborative, deadline-driven environments make me a strong addition to any data analysis team. I have experience working in large and small scale teams, with either all team members present or working remotely. While not working on projects or repos on Github, I spend my time learning, practicing and updating my skills and languages.",
+    "website": "https://cryptic-springs-39777.herokuapp.com/",
+    "education":[
+        {
+            "UniversityName" : "Northwestern Univerisity School of Professional Studies",
+            "MonthStarted": "December",
+            "YearStarted": "2019",
+            "MonthGraduated": "March",
+            "YearGraduated": "2020",
+            "Achievements": "A 24-week intensive program focused on gaining technical programming skills in HTML5, CSS3, Javascript, JQuery, Bootstrap, Firebase, Node Js, MySQL, MongoDB, Express, Handelbars.js & ReactJS."
+        },
+        {
+            "UniversityName" : "Eastern Illinois University",
+            "MonthStarted": "August",
+            "YearStarted": "2014",
+            "MonthGraduated": "May",
+            "YearGraduated": "2016",
+            "Achievements": "Society for Collegiate Journalist member, Daniel E. and M. Adrianne Thornburgh Scholarship	recipient"
+        }
+    ],
+    "work": [
+        {
+            "CompanyName":"Grubhub",
+            "JobTitle": "Data Entry Specialist",
+            "MonthBegin": "March",
+            "YearBegin": "2017",
+            "MonthEnd": "October",
+            "YearEnd" : "2019",
+            "JobDescription": "Worked on multiple deadline-driven projects with large and small scale teams to deliver high standards of quality work within 48 hours. Maintained constant communication with teammates and with restaurants and other clients via email and over the phone. Quickly learned multiple new systems within the company to increase my work flow ability."
+        },
+        {
+            "CompanyName":"Gary Comer College Prep Middle School",
+            "JobTitle": "Enrollment Support Specialist",
+            "MonthBegin": "August",
+            "YearBegin": "2016",
+            "MonthEnd": "October",
+            "YearEnd" : "2016",
+            "JobDescription": "Helped organize large scale student databases, both physical and online, to help prepare for the upcoming school year. Assisted staff members with administrative duties, including office work and communication with student families to inform them of student happenings and important school year details."
+        },
+        {
+            "CompanyName":"The Daily Eastern News",
+            "JobTitle": "News Editor",
+            "MonthBegin": "January",
+            "YearBegin": "2016",
+            "MonthEnd": "May",
+            "YearEnd" : "2016",
+            "JobDescription": "Led a small staff of editors and reporters and worked with them to plan daily news stories throughout the week. Worked on daily deadlines to write and edit at minimum ten stories and photos. Collaborated with rotating reporters and helped them find their own stories to work on. Covered multiple large-scale events including the new selection of the new university president."
+        }
+    ],
+    "skillsDescription": "Web Devloper with a background in Journalism and experience in working with front-end, back-end and full stack web development. Efficient in working independently and with teams across multiple tasks and in problem-solving to develop functioning applications. Experience in working in collaborative, deadline-driven environments.",
+    "skills": [
+        {
+            "skillname": "HTML5"
+        },
+        {
+            "skillname": "CSS"
+        },
+        {
+            "skillname": "Javascript"
+        },
+        {
+            "skillname": "jQuery"
+        },
+        {
+            "skillname": "Bootstrap"
+        },
+        {
+            "skillname": "API"
+        },
+        {
+            "skillname": "MongoDB/Mongoose"
+        },
+        {
+            "skillname" : "MySQL/Sequelize"
+        },
+        {
+            "skillname": "MVC"
+        },
+        {
+            "skillname":"Heroku Deployment"
+        },
+        {
+            "skillname": "Git"
+        },
+        {
+            "skillname": "C++"
+        },
+        {
+            "skillname": "Python"
+        },
+        {
+            "skillname": "Vue.js"
+        },
+        {
+            "skillname": "React.js"
+        },
+        {
+            "skillname": "Node.js"
+        },
+        {
+            "skillname": "Express"
+        }
+    ],
+    "portfolio":[
+        {
+            "name": "KnightFall",
+            "description": "A Choose Your Own Story RPG game",
+            
+        },
+        {
+            "name":"Titanic: An Educational Experience",
+            "description": "An educational look onboard the RMS Titanic"
+        },
+        {
+            "name": "Dinner and A Movie",
+            "description": "A date night app to help plan the perfcet night out",
         }
     ]
 };
